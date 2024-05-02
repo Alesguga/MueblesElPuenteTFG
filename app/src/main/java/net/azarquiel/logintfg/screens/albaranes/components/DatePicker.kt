51325d304ec80267c2c8644c5bdb669b218b99f4
Alejandro Gutiérrez Galán <alexgutigalan04@gmail.com>
2024-05-02 11:06:46 +0200

    Mejora tamaños

--- app/src/main/java/net/azarquiel/logintfg/screens/albaranes/components/DatePicker.kt
+++ app/src/main/java/net/azarquiel/logintfg/screens/albaranes/components/DatePicker.kt
@@ -12,13 +12,17 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import net.azarquiel.logintfg.screens.login.components.MueblesElPuenteAppTFGTheme
 import net.azarquiel.logintfg.ui.theme.grisC
 import net.azarquiel.logintfg.ui.theme.grisCC
+import net.azarquiel.logintfg.ui.theme.grisO
+import net.azarquiel.logintfg.ui.theme.naranjaMEP
 import java.time.LocalDate
 import java.time.YearMonth
 import java.time.format.TextStyle
@@ -34,49 +38,72 @@ fun CalendarioMes(mes: YearMonth = YearMonth.now(), diasOcupados: List<LocalDate
         DiaCalendario(fecha, diasOcupados.contains(fecha))
     }
 
-    Column(modifier = Modifier.padding(8.dp)) {
-        // Encabezado del calendario
-        Text(text = "${mes.month.getDisplayName(TextStyle.FULL, Locale.getDefault())} ${mes.year}",
-            style = MaterialTheme.typography.headlineSmall,
-            modifier = Modifier.align(Alignment.CenterHorizontally))
+        Column(modifier = Modifier
+            .padding(8.dp)
+            .clip(RoundedCornerShape(10.dp))
+            .background(grisO)
+        ) {
+            Text(
+                text = "${
+                    mes.month.getDisplayName(
+                        TextStyle.FULL,
+                        Locale.getDefault()
+                    )
+                } ${mes.year}",
+                style = MaterialTheme.typography.bodyLarge,
+                fontSize = 25.sp,
+                modifier = Modifier
+                    .padding(8.dp)
+                    .align(Alignment.CenterHorizontally),
+                color = naranjaMEP
+            )
 
-        // Días de la semana
-        Row(modifier = Modifier.fillMaxWidth()) {
-            listOf("L", "M", "X", "J", "V", "S", "D").forEach { dia ->
-                Text(text = dia, modifier = Modifier
-                    .padding(4.dp)
-                    .weight(1f),
-                    textAlign = TextAlign.Center)
-            }
-        }
-
-        // Filas del calendario
-        val semanas = diasConEstado.chunked(7)
-        semanas.forEach { semana ->
+            // Días de la semana
             Row(modifier = Modifier.fillMaxWidth()) {
-                semana.forEach { dia ->
-                    Box(
-                        contentAlignment = Alignment.Center,
-                        modifier = Modifier
-                            .weight(1f)
-                            .padding(4.dp)
-                            .border(BorderStroke(1.dp,grisC), RoundedCornerShape(4.dp))
-                            .background(if (dia.esOcupado) Color.Red else Color.Transparent)
-                            .clickable { onDiaSeleccionado(dia.fecha) }
-                    ) {
-                        Text(text = dia.fecha.dayOfMonth.toString(),
-                            modifier = Modifier
-                                .padding(5.dp)
-                                .background(if (dia.esOcupado) Color.Red else Color.Transparent),
-                            textAlign = TextAlign.Center,
+                listOf("L", "M", "X", "J", "V", "S", "D").forEach { dia ->
+                    Text(
+                        text = dia, modifier = Modifier
+                            .padding(8.dp)
+                            .weight(1f),
+                        textAlign = TextAlign.Center,
+                        color = grisCC
+                    )
+                }
+            }
 
-                        )
+            // Filas del calendario
+            val semanas = diasConEstado.chunked(7)
+            semanas.forEach { semana ->
+                Row(modifier = Modifier.fillMaxWidth()) {
+                    semana.forEach { dia ->
+                        Box(
+                            contentAlignment = Alignment.Center,
+                            modifier = Modifier
+                                .weight(1f)
+                                .padding(4.dp)
+                                .border(BorderStroke(1.dp, grisC), RoundedCornerShape(4.dp))
+                                .background(if (dia.esOcupado) naranjaMEP else Color.Transparent)
+                                .clickable { onDiaSeleccionado(dia.fecha) }
+                        ) {
+                            Text(
+                                text = dia.fecha.dayOfMonth.toString(),
+                                modifier = Modifier
+                                    .padding(6.dp)
+                                    .background(if (dia.esOcupado) naranjaMEP else Color.Transparent),
+                                textAlign = TextAlign.Center,
+                                color = if (dia.esOcupado) Color.White else grisCC
+                            )
+                            Spacer(modifier = Modifier.padding(20.dp))
+                        }
                     }
                 }
             }
+            Spacer(modifier = Modifier.padding(4.dp))
+
         }
     }
-}
+
+
 @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
 @Preview (showBackground = true)
 @Composable
